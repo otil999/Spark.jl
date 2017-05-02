d670db756f425da3ae2beafb2f6baa19ebe4b69e
Kalman Jantner <jantner@gmail.com>
2017-05-02 15:53:09 +0200

    read spark properties from spark-defaults.conf file

--- src/config.jl
+++ src/config.jl
@@ -9,6 +9,7 @@ function SparkConf(;opts...)
     for (k, v) in opts
         jcall(jconf, "set", JSparkConf, (JString, JString), string(k), v)
     end
+    spark_defaults(jconf)
     return SparkConf(jconf)
 end
     
@@ -40,4 +41,28 @@ end
 
 function setappname(conf::SparkConf, appname::AbstractString)
     jcall(conf.jconf, "setAppName", JSparkConf, (JString,), appname)
+end
+
+
+function isnotcomment(x)
+    return !startswith("#", x)
+end
+
+
+function spark_defaults(jconf::JSparkConf)
+    sconf = get(ENV, "SPARK_CONF_DIR", "")
+    if sconf == ""
+        shome =  get(ENV, "SPARK_HOME", "")
+        if shome == "" ; return jconf; end
+        sconf = joinpath(shome, "conf")
+    end
+    p = map(split, filter(isnotcomment, split(readstring(joinpath(sconf, "spark-defaults.conf")), '\n', keep=false) ) )
+    for x in p
+         jcall(jconf, "set", Spark.JSparkConf, (JString, JString), x[1], x[2])
+    end
+    return jconf
+end
+
+function setdeploy(conf::SparkConf, deploymode::AbstractString)
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "deploy-mode", deploymode)
 end
