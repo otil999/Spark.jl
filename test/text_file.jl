cb2baf193cec469de558995c0093fecc157305b8
Attila Szucs <attila_szucs@epam.com>
2017-05-12 10:58:41 +0200

    fix text_file test

--- test/text_file.jl
+++ test/text_file.jl
@@ -1,10 +1,9 @@
 # test of text_file
 sc = SparkContext(master="local")
 
-txt = text_file(sc, @__FILE__)
+txt = text_file(sc, "file://" * @__FILE__)
 nums  = map(txt, it -> length(it))
 
-@test reduce(nums, +) == 153
+@test reduce(nums, +) == 165
 
 close(sc)
-
