907372c8dcc8807263036112cd35c21d8a51f8df
Kalman Jantner <kalman.jantner@aviva.com>
2017-07-11 15:01:24 +0200

    Avik's performance fix

--- jvm/sparkjl/src/main/scala/org/apache/spark/api/julia/JuliaRDD.scala
+++ jvm/sparkjl/src/main/scala/org/apache/spark/api/julia/JuliaRDD.scala
@@ -218,17 +218,22 @@ object JuliaRDD extends Logging {
     rdd1.cartesian(rdd2)
   }
 
-  def collectToByteArray[T](javaCollected: java.util.List[T]): Array[Byte] = {
+  def writeToByteArray[T](obj: Any): Array[Byte] = {
     val byteArrayOut = new ByteArrayOutputStream()
     val dataStream = new DataOutputStream(byteArrayOut)
-    writeValueToStream(javaCollected, dataStream)
+    writeValueToStream(obj, dataStream)
     dataStream.flush()
     byteArrayOut.toByteArray()
   }
   
   def collectToJulia(rdd: JavaRDD[Any]): Array[Byte] = {
-    collectToByteArray[Any](rdd.collect())
+    writeToByteArray[java.util.List[Any]](rdd.collect())
   }
+
+  def collectToJuliaItr(rdd: JavaRDD[Any]): java.util.List[Any] = {
+    return rdd.collect()
+  }
+
 }
 
 class JuliaPairRDD(@transient parent: RDD[_],command: Array[Byte]) extends AbstractJuliaRDD[(Any, Any)](parent, command) {
@@ -243,6 +248,9 @@ object JuliaPairRDD extends Logging {
     new JuliaPairRDD(rdd, command)
 
   def collectToJulia(rdd: JavaPairRDD[Any, Any]): Array[Byte] = {
-    JuliaRDD.collectToByteArray[(Any, Any)](rdd.collect())
+    JuliaRDD.writeToByteArray[java.util.List[(Any, Any)]](rdd.collect())
+  }
+  def collectToJuliaItr(rdd: JavaPairRDD[Any, Any]): java.util.List[(Any, Any)] = {
+    return rdd.collect()
   }
 }
